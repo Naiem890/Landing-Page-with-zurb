6fe425f95aa866570f2f1e6b59fdd521ee516419
Naiem890 <solaiman.naiem890@gmail.com>
2022-03-15 13:32:23 +0600

    hero + company logos animation added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation();
+gsap.registerPlugin(ScrollTrigger);
+const tl = gsap.timeline();
+
+gsap.from(".hero-content", { x: -700, duration: 1 });
+gsap.from(".hero-img", { x: 700, duration: 1 });
+gsap.from(".company-logo", {
+  y: 100,
+  duration: 1,
+  opacity: 0.1,
+  stagger: 0.15,
+  ease: Circ.easeOut,
+  scrollTrigger: "#featureSection",
+});
